988672640ad228bde5cb83fa710a58e2a0dada45
J.C. Hiatt <jc@jchiatt.com>
2021-09-03 16:16:48 -0500

    wip: stub commands we want

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -16,6 +16,18 @@ export class Client implements Client {
     this.socket = socket;
   }
 
+  broadcast(input: string) {
+    Object.entries(this.chatServer.clients).forEach(([username, client]) => {
+      // broadcast message to all connected clients
+      if (client !== this) {
+        client.writeLine(`${new Date().toLocaleString()} > ${username} -- ${input}`);
+      } else {
+        // change output format to indicate that *you* sent this message
+        client.writeLine(`${new Date().toLocaleString()} > You (${username}) -- ${input}`);
+      }
+    });
+  }
+
   greet() {
     this.writeLine(`Welcome ${this.auth.username}. ${this.chatServer.clientCount} users are here.`);
   }
@@ -25,6 +37,22 @@ export class Client implements Client {
     const [command, ...params] = input.slice(1).split(" ");
 
     switch (command) {
+      case "CHANNELS": {
+        // /CHANNELS - List available channels.
+        return;
+      }
+      case "JOIN": {
+        // /JOIN CHANNEL <channel name> - Join a channel, create if it doesn't exist.
+        return;
+      }
+      case "KICK": {
+        // /KICK <channel name> <username> - Kick a user from a channel you're an admin of.
+        return;
+      }
+      case "LEAVE": {
+        // /LEAVE CHANNEL <channel name> - Leave a channel.
+        return;
+      }
       case "LOGIN": {
         // /LOGIN <desired username> - Authenticate with the server.
         this.auth.login(params.join(" "));
@@ -35,9 +63,29 @@ export class Client implements Client {
         this.auth.logout();
         return;
       }
+      case "MEMBER": {
+        // /MEMBER - List channels you've joined.
+        return;
+      }
+      case "NICK": {
+        // /NICK <new username> - Change your username.
+        return;
+      }
       case "HELP": {
         // /HELP - List available commands.
         this.listCommands();
+        return;
+      }
+      case "HERE": {
+        // /HERE <channel name> - List users in this channel. List global users if no channel provided.
+        return;
+      }
+      case "UPDATE": {
+        // /UPDATE CHANNEL <channel name> - Update channel name for a channel you're an admin of.
+        return;
+      }
+      case "WHISPER": {
+        // /WHISPER <username> - Send a private message.
         return;
       }
       default: {
@@ -57,6 +105,8 @@ export class Client implements Client {
       this.writeLine("You are not authenticated. Type /AUTH to authenticate.");
       return;
     }
+
+    this.broadcast(input);
   }
 
   listCommands() {
@@ -64,9 +114,18 @@ export class Client implements Client {
       #####################################################################################################
       # COMMANDS
       #
+      # /CHANNELS - List available channels.
+      # /JOIN CHANNEL <channel name> - Join a channel, create if it doesn't exist.
+      # /KICK <channel name> <username> - Kick a user from a channel you're an admin of.
+      # /LEAVE CHANNEL <channel name> - Leave a channel.
       # /LOGIN <desired username> - Authenticate with the server.
       # /LOGOUT - Log out.
+      # /MEMBER - List channels you've joined.
+      # /NICK <new username> - Change your username.
       # /HELP - List available commands.
+      # /HERE <channel name> - List users in this channel. List global users if no channel provided.
+      # /UPDATE CHANNEL <channel name> - Update channel name for a channel you're an admin of.
+      # /WHISPER <username> - Send a private message.
       #####################################################################################################
     `);
   }
