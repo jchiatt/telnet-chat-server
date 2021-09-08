a455cc2d8b3ffe8390de5adcc3b0d68049e64da1
J.C. Hiatt <jc@jchiatt.com>
2021-09-07 23:19:26 -0500

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import http from "http";
 import { initConfig } from "./config/initConfig";
 import { setupLogging } from "./helpers/logToFile";
 import { ChatServer } from "./server/ChatServer";
@@ -24,3 +25,52 @@ chatServer.onError(handleError);
 
 // Listen for connections on port specified in config
 chatServer.listen();
+
+// Set up HTTP server
+const httpHost = "localhost";
+const httpServerPort = 8123;
+
+const requestListener = function (req: any, res: any) {
+  res.setHeader("Content-Type", "text/plain");
+  switch (req.url) {
+    case "/login":
+      if (req.method !== "POST") {
+        res.writeHead(400);
+        res.end("You must send a POST with your desired username.");
+        break;
+      }
+
+      let body = "";
+
+      req.on("data", (chunk: Buffer) => {
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {
+        console.log(body);
+
+        if (body.split(" ").length > 1) {
+          res.writeHead(400);
+          res.end("Username cannot contain spaces");
+        }
+
+        res.writeHead(200);
+        res.end(`login with ${body}`);
+      });
+
+      break;
+    case "/logout":
+      res.writeHead(200);
+      res.end("logout");
+      break;
+    default:
+      res.writeHead(404);
+      res.end("Not found");
+  }
+};
+
+const httpServer = http.createServer(requestListener);
+
+httpServer.listen(httpServerPort, httpHost, () => {
+  console.log(`HTTP server is running on http://${httpHost}:${httpServerPort}`);
+});
